fdd7d003f723ba544c0895ad2cd234e123015365
pabvald <valdunciel98@gmail.com>
2018-12-20 23:03:12 +0100

    Añadidas consultas 5, 6 y 7. Añadida foto de esquema. Separados enunciados.

--- consultas.sql
+++ consultas.sql
@@ -1,22 +1,23 @@
--- Titulares con licencias activas
+--  1 --
 SELECT T.nombre
 FROM titular T NATURAL JOIN concesion C
 WHERE C.fechaF IS NULL;
 
--- Historial de licencias de un puesto dado. Ej: Puesto 2
+-- 2 --
 SELECT DISTINCT P.nro,C.dni,C.fechaI,C.fechaF
 FROM puesto P NATURAL JOIN concesion C
 WHERE P.nro = 2
 ORDER BY C.fechaI ASC;
 
--- Número de licencias que ha tenido cada puesto:
--- Incluyendo las activas
+-- 3 -- 
 SELECT P.nro, COUNT(*) AS numL
 FROM puesto P NATURAL JOIN concesion C
 GROUP BY P.nro;
--- Sin incluir las activas. Habría que añadir la siguiente cláusula
--- WHERE c.fechaF IS NOT NULL
--- Persona/s (dni, nombre) que al menos han tenido 2 infracciones
+
+-- 4 --
+    --Habría que añadir la siguiente cláusula
+    -- WHERE c.fechaF IS NOT NULL
+    -- Persona/s (dni, nombre) que al menos han tenido 2 infracciones
 WITH NumInf AS (SELECT DISTINCT C.dni AS dni, COUNT(*) as numI
                 FROM concesion C NATURAL JOIN sancion S
                 GROUP BY C.dni
@@ -24,3 +25,63 @@ WITH NumInf AS (SELECT DISTINCT C.dni AS dni, COUNT(*) as numI
                 
 SELECT T.nombre, NI.dni,NI.numI
 FROM titular T NATURAL JOIN NumInf NI;
+
+
+-- 5 -- 
+    -- * Planteamiento 
+
+        -- concActivasLimpias : Concesiones con fechaF IS null  y cod NOT IN Sancion.cod
+        -- Nombre del Titular con dni IN concActivasLimpias.dni
+
+    -- * SQL 
+
+    WITH ConcActivasLimpias AS ( SELECT C.dni 
+                                 FROM Concesion C
+                                 WHERE C.fechaF IS null 
+                                       AND C.cod NOT IN (SELECT S.cod FROM Sancion S))
+    
+    SELECT T.nombre
+    FROM Titular T natural join ConcActivasLimpias CAL;
+
+-- 6 --
+    -- * Planteamiento
+
+        -- ConcActivas : Concesiones con fechaF IS null
+        -- Nsanciones : COUNT(*) Titular con dni = ConcActivas.dni, Sanciones con cod = ConcActivas.cod , GRUOP BY dni
+        -- Comparar Nsanciones con Nsanciones y quedarse con las tuplas que verifiquen que el número de tuplas con mas sanciones
+        -- es menor que 5.
+
+    -- * SQL 
+
+    WITH ConcActivas AS (SELECT * 
+                         FROM Concesion C 
+                         WHERE C.fechaF IS null),
+
+    Nsanciones AS (SELECT T.dni,T.nombre, COUNT(*) as nsan
+                   FROM Titular T, ConcActivas CA, Sancion S 
+                   WHERE T.dni = CA.dni AND S.cod = CA.cod
+                   GROUP BY T.dni)
+                
+    SELECT Ns.nombre, Ns.nsan
+    FROM Nsanciones Ns 
+    WHERE 5 > (SELECT COUNT(*)
+               FROM Nsanciones Ns2
+               WHERE Ns2.nsan > Ns.nsan);
+
+-- 7 --
+    --* Planteamiento 
+
+        -- SELECT C.nro, C.fechaI, C.fechaF, T.nombre 
+        -- C.dni = T.dni
+        -- ORDER BY C.nro DESC, C.fechaI ASC
+
+    --* SQL
+
+    SELECT C.nro as Puesto, C.fechaI as Fecha_inicio, C.fechaF as Fecha_fin, T.nombre
+    FROM Concesion C, Titular T 
+    WHERE C.dni = T.dni 
+    ORDER BY C.nro DESC, C.fechaI ASC;
+
+
+
+
