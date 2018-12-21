6ecebf8b33d62e5b8dac4a51c7c22f584718d1bd
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2018-12-21 11:33:24 +0100

    Consultas relacionadas con sanciones

--- consultas.sql
+++ consultas.sql
@@ -24,3 +24,30 @@ WITH NumInf AS (SELECT DISTINCT C.dni AS dni, COUNT(*) as numI
                 
 SELECT T.nombre, NI.dni,NI.numI
 FROM titular T NATURAL JOIN NumInf NI;
+
+-- Titulares con sancion en el año 2011 
+SELECT T.nombre,COUNT(*) AS num_sanciones
+FROM Titular T
+	JOIN Concesion C ON T.dni = C.dni
+	JOIN Sancion S ON C.cod = S.cod
+WHERE
+	'2011-12-31' >= S.fecha AND
+	'2011-01-01' <= S.fecha
+GROUP BY T.nombre;
+
+-- Puesto con mas sanciones
+WITH SancionesPuesto AS (
+	SELECT P.nro,COUNT(*) as num_sanciones
+	FROM Puesto P
+		NATURAL JOIN Concesion
+		NATURAL JOIN Sancion
+	GROUP BY P.nro
+)
+SELECT SP.nro, SP.num_sanciones
+FROM SancionesPuesto SP
+WHERE
+	SP.num_sanciones >= ALL(
+		SELECT SP2.num_sanciones
+		FROM SancionesPuesto SP2
+	)
+;
